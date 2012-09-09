2d69ab104ec1965efb229b7132493ec9b3d72701
Juhamatti Niemelä <iiska@iki.fi>
2012-09-09 16:27:05 +0300

    Basic password entry importing done

---
+++ README.md
@@ -0,0 +1,17 @@
+Script for importing password data XML from
+[KeepassX](https://www.keepassx.org/) to
+[password-store](http://zx2c4.com/projects/password-store/).
+
+## Usage
+
+1. Select File --> Export --> KeepassX XML from KeepassX menu to export password data.
+2. Run script keepassx2pass.py with the exported filename as an argument
+
+    keepassx2pass.py your-export-file.py
+
+
+## TODO
+
+At least following usefull features are missing
+
+* Import KeepassX comments and other addtional info as multiline passwords
--- keepassx2pass.py
+++ keepassx2pass.py
@@ -1,23 +1,37 @@
 #! /usr/bin/env python
 
+import sys
+
+from subprocess import Popen, PIPE
 from xml.etree import ElementTree
-from sys import argv
+
+def path_for(element, path=''):
+    """ Generate path name from elements title and current path """
+    title = element.find('title').text
+    return '/'.join([path, title])
 
 def import_entry(element, path=''):
-    print element
-    print element.find('title').text
+    """ Import new password entry to password-store using pass insert
+    command """
+    proc = Popen(['pass', 'insert', path_for(element, path)],
+              stdin=PIPE, stdout=PIPE)
+    proc.communicate(element.find('password').text + "\n")
+    proc.wait()
 
-def import_group(element):
-    print element
-    print element.find('title').text
-    for g in element.findall('group'):
-        import_group(g)
-    for e in element.findall('entry'):
-        import_entry(e)
+def import_group(element, path=''):
+    """ Import all entries and sub-groups from given group """
+    npath = path_for(element, path)
+    for group in element.findall('group'):
+        import_group(group, npath)
+    for entry in element.findall('entry'):
+        import_entry(entry, npath)
 
+
+def main(xml_file):
+    """ Parse given KeepassX XML file and import password groups from it """
+    with open(xml_file) as xml:
+        for group in ElementTree.XML(xml.read()).findall('group'):
+            import_group(group)
 
 if __name__ == '__main__':
-    with open(argv[1]) as f:
-        doc = ElementTree.XML(f.read())
-        for g in doc.findall('group'):
-            import_group(g)
+    main(sys.argv[1])
